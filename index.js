5d0a390a4a10ec48b752182f74d49ddf8a72109a
JuanC Riera <jc.rierag@gmail.com>
2019-02-12 19:59:55 -0600

    cree mi propia rama

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cowsay = require('cowsay');
 const intro = require('./intro');
 const nudo = require('./nudo');
 const final = require('./final');
+const fs = require('fs')
 
 const personaje = "Vaquita";
 const edad = Math.floor(Math.random() * 100);
@@ -10,8 +11,15 @@ const villano = "Suegra";
 const lugar = "Ningún lugar";
 const hobby = "Programar";
 
-console.log(intro.historia(personaje, edad, lugar));
-console.log(nudo.nudo(personaje,edad,villano,lugar,hobby));
+const introHistoria = intro.historia(personaje, edad, lugar);
+const nudoHistoria = nudo.nudo(personaje,edad,villano,lugar,hobby);
+const finalHistoria = final.historia(personaje, edad, viajeEnElTiempo, villano, lugar, hobby);
+
+const historiaCompleta = introHistoria + nudoHistoria + finalHistoria;
+
+console.log(historiaCompleta);
+
+//console.log(nudo.nudo(personaje,edad,villano,lugar,hobby));
 console.log(cowsay.say({
   text : 'hola',
   e : "x-o",
